32ad60f9810e86d9eaca90513807db6d8953336d
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-02-26 15:40:42 +0200

    feat: bd access

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ dotenv.config({ path: "./config/config.env" })
 connectDB()
 const PORT = process.env.PORT || 5000
 const app = express()
+app.use(express.json())
 app.use("/api/v1/transactions", transaction)
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 )
